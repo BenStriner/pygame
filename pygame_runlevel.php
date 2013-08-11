3e0451a32b8a3dbb5455eda84ee5fc4d34fb83e8
bstriner <bstriner@yahoo.com>
2013-08-11 14:01:05 -0400

    fixes

--- pygame_runlevel.php
+++ pygame_runlevel.php
@@ -2,7 +2,7 @@
 
 //Adds content when viewing a level
 
-function pygame_node_level_view($node, $view_mode, $langcode){
+function pygame_node_level_view($node, $view_mode){
 	if($view_mode == 'full'){
 		$node->content['pygame_leveldisplay']=array(
 			'#theme' => 'pygame_level_display_initial',
@@ -11,23 +11,27 @@ function pygame_node_level_view($node, $view_mode, $langcode){
 		$state = array('node_id' => $node->nid);
 		$node->content['pygame_codeform']=drupal_build_form('pygame_node_level_codeform', $state);
 	}
+	return $node;
 }
 
 //Initial view of level
 function theme_pygame_level_display_initial($variables){
-	//	$level = pygame_level_get($variables['node']);
-	//	return pygame_level_render($level);
-	return "<p>Test Level Node: ".$variables['node']->nid."</p>";
+		$level = pygame_level_get($variables['element']['node']);
+		return pygame_level_render($level);
+//	return "<p>Test Level Node: ".$variables['element']['node']->nid."</p><p>".print_r($variables)."</p>";
+//return pygame_level_render(array('testkey'=>'testval'));
 }
 
 //Initial level object
 function pygame_level_get($node){
 	$dict = array(
-		'code'=>$node->pygame_node_level_code_generate,
+		'code'=>$node->pygame_node_level_code_generate['und'][0]['value'],
 		'data'=>array('map'=>array(),'players'=>array())
 	);
 	$level = pygame_run_script_with_input($dict);
 	return $level;
+	//return $dict;
+//	return array("tk"=>"tv");
 }
 
 //Returns the form for entering code
@@ -57,24 +61,17 @@ function pygame_node_level_codeform($form, &$form_state) {
 
 #Renders a level dictionary
 function pygame_level_render($level){
+//	return "<p>".print_r($level,true)."</p>";
 	$ret = "<div class='csstable'>";
-	for($y=0;$y<count($level['map']);$y++){
+	for($y=0;$y<count($level->map);$y++){
 		$ret .= "<div class='csstr'>";
-		for($x=0;$x<count($level['map'][$y]);$x++){
+		for($x=0;$x<count($level->map[$y]);$x++){
 			$ret .= "<div class='csstd'>";
-			$cell = $level['map'][$y][$x];
+			$cell = $level->map[$y][$x];
 			$tilenode = node_load($cell);
 			$img = $tilenode->pygame_node_tile_image['und'][0]['uri'];
 			$uri = file_create_url($img);
 			$ret .= '<img src="'.$uri.'" />';
-			/*if($this->player_x == $x && $this->player_y == $y){
-				$ret .= "<div class='csstd' style='background-image:url(/sites/all/themes/pygame_theme/sprite.jpg)'><br/></div>";
-//				$ret .= '<img src="/sites/all/themes/pygame_theme/sprite.jpg" class="arenatile" />';
-			}else{
-				$ret .= "<div class='csstd' style='background-image:url(/sites/all/themes/pygame_theme/background.jpg)'><br/></div>";
-				//$ret .= '<img src="/sites/all/themes/pygame_theme/background.jpg" class="arenatile" />';
-			}*/
-			$ret .= $cell;
 			$ret .= "</div>";
 		}
 		$ret .= "</div>";
@@ -82,40 +79,6 @@ function pygame_level_render($level){
 	$ret .= "</div>";
 	return $ret;	
 }
-
-/*
-class AIArena{
-	public $height = 20;
-	public $width = 20;
-	public $player_x = 10;
-	public $player_y = 10;
-	public function to_script(){
-		return "";
-	}
-	public function render(){
-	
-	
-		$ret = "<div class='csstable'>";
-		for($y=0;$y<$this->height;$y++){
-			$ret .= "<div class='csstr'>";
-			for($x=0;$x<$this->width;$x++){
-				//$ret .= "<div class='csstd'>";
-				if($this->player_x == $x && $this->player_y == $y){
-					$ret .= "<div class='csstd' style='background-image:url(/sites/all/themes/pygame_theme/sprite.jpg)'><br/></div>";
-//					$ret .= '<img src="/sites/all/themes/pygame_theme/sprite.jpg" class="arenatile" />';
-				}else{
-					$ret .= "<div class='csstd' style='background-image:url(/sites/all/themes/pygame_theme/background.jpg)'><br/></div>";
-					//$ret .= '<img src="/sites/all/themes/pygame_theme/background.jpg" class="arenatile" />';
-				}
-				//$ret .= "</div>";
-			}
-			$ret .= "</div>";
-		}
-		$ret .= "</div>";
-		return $ret;	
-	}
-}
-*/
 
 //Called when user enters code and hits run
 //Returns AJAX commands to animate the level
@@ -220,7 +183,7 @@ Runs "python_runner.py" python script
 Decodes script output as JSON
 */
 function pygame_run_script_with_input($input_dict){
-	$input = json_encode($inputs);
+	$input = json_encode($input_dict);
 	$ifile = file_save_data($input);
 	$path = drupal_realpath(drupal_get_path('module','pygame').'/python_runner.py');
 	$ipath = drupal_realpath($ifile->uri);
